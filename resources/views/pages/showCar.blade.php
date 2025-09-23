c76095e4974b7915e21e71f72f2d036a8d7001d3
mohamed fathy <mohamed@fathy.com>
2025-09-23 23:43:36 +0200

    شس

--- resources/views/pages/showCar.blade.php
+++ resources/views/pages/showCar.blade.php
@@ -89,32 +89,6 @@
                             </div>
                         </div>
 
-                        <!-- Pricing -->
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingPrice">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapsePrice">
-                                    Pricing
-                                </button>
-                            </h2>
-                            <div id="collapsePrice" class="accordion-collapse collapse"
-                                data-bs-parent="#accordionCarDetails">
-                                <div class="accordion-body">
-                                    <div class="row g-3">
-                                        <div class="col-md-4"><strong>Original Price:</strong>
-                                            {{ $car['pricing']['original_price_formatted'] }}</div>
-                                        <div class="col-md-4"><strong>Discount:</strong>
-                                            {{ $car['pricing']['discount_formatted'] }}</div>
-                                        <div class="col-md-4"><strong>Final Price:</strong>
-                                            {{ $car['pricing']['final_price_formatted'] }}</div>
-                                        <div class="col-md-4"><strong>Monthly Installment:</strong>
-                                            {{ $car['pricing']['monthly_installment_formatted'] ?? 'N/A' }}</div>
-                                        <div class="col-md-4"><strong>Down Payment:</strong>
-                                            {{ $car['pricing']['down_payment'] ?? 'N/A' }}</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
                         <!-- Trim -->
                         <div class="accordion-item">
@@ -163,94 +137,6 @@
                             </div>
                         </div>
 
-                        <!-- Flags, Features, Conditions -->
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingExtras">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseExtras">
-                                    Flags, Features & Conditions
-                                </button>
-                            </h2>
-                            <div id="collapseExtras" class="accordion-collapse collapse"
-                                data-bs-parent="#accordionCarDetails">
-                                <div class="accordion-body">
-                                    <div class="row g-3">
-                                        <div class="col-12">
-                                            <strong>Flags:</strong>
-                                            <ul>
-                                                @foreach ($car['flags'] as $flag)
-                                                    <li>
-                                                        @if (isset($flag['image']))
-                                                            <img src="{{ asset('storage/' . $flag['image']) }}"
-                                                                alt="Flag Image"
-                                                                style="height: 24px; width: auto; margin-right: 8px;">
-                                                        @endif
-                                                        {{ $flag['value_en'] . ',' }}
-                                                        {{ $flag['value_ar'] }}
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-
-                                            <strong>Features:</strong>
-                                            <ul>
-                                                @foreach ($car['features'] as $name => $featuresGroup)
-                                                <strong>{{ ucwords(str_replace('_', ' ', $name)) }}</strong>
-                                                    @foreach ($featuresGroup as $feature)
-                                                    <li>
-                                                        <ul>
-
-                                                            <li>
-                                                                <strong>{{ $feature['label_en'] }}</strong>:
-                                                                {{ $feature['value_en'] }}
-                                                            </li>
-
-                                                            <li>
-                                                                <strong>{{ $feature['label_ar'] }}</strong>:
-                                                                {{ $feature['value_ar'] }}
-                                                            </li>
-                                                        </ul>
-                                                    </li>
-                                                        
-                                                    @endforeach
-                                                @endforeach
-                                            </ul>
-
-
-                                            <strong>Conditions:</strong>
-                                            <ul>
-                                                @foreach ($car['conditions'] as $name => $condtion)
-                                                {{-- @dd($car['conditions'] , $condtion , $name) --}}
-                                                    <li>
-                                                        <strong>
-                                                            {{ \App\Enums\Condition::tryFrom($name)?->label() ?? ucfirst(str_replace('_', ' ', $name)) }} 
-                                                            {{-- {{ \App\Enums\Condition::tryFrom($name)?->label() ?? ucfirst(str_replace('_', ' ', $condition['name_ar'])) }} --}}
-                                                        </strong>
-                                                        <ul>
-                                                        @foreach ($condtion as $cond )
-                                             
-                                                            <li style="list-style-type: none; margin-left: 1rem;">
-                                                                @if (!empty($cond['image']))
-                                                                    <img src="{{ asset('storage/' . $cond['image']) }}"
-                                                                        alt="Condition Image"
-                                                                        style="height: 24px; width: auto; margin-right: 8px;">
-                                                                @endif
-                                                                <strong>{{ $cond['part_en']}} , {{ $cond['part_ar'] }} : </strong>
-                                                                <br/>
-                                                                <strong>{{ $cond['description_en'] }} , {{ $cond['description_ar']}}</strong> 
-                                                            </li>
-
-                                                            @endforeach
-                                                        </ul>
-                                                   
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
                     </div>
 
